fef82f74b6d55f068fb2a20f42b28cb5c2a855d4
KarthikNarsapur <karthik.n@cloudthat.com>
2025-04-22 14:04:26 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "instance_name" {
+  type        = string
+  description = "Name of the EC2 instance"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Type of EC2 instance"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "AMI ID to use for the instance"
+  default     = "ami-0e35ddab05955cf57"
+}
+
+variable "security_group" {
+  type        = string
+  description = "Security group name"
+}
+
+variable "allowed_ports" {
+  type        = list(number)
+  description = "List of allowed inbound ports"
+}
+
+variable "enable_public_ip" {
+  type        = bool
+  description = "Whether to associate a public IP"
+  default     = true
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID to launch instance in"
+  default     = "vpc-0822634396310e8ea"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "Subnet ID to launch instance in"
+  default     = "subnet-0376b05408a1be5d4"
+}
