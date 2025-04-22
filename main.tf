742315ff7cfa73afe855696a5106b6785b9b74e9
KarthikNarsapur <karthik.n@cloudthat.com>
2025-04-22 14:01:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = var.security_group
+  description = "Security group for EC2 instance"
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.allowed_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name       = "${var.instance_name}-sg"
+    CreatedBy  = "Backstage"
+  }
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = var.enable_public_ip
+
+  tags = {
+    Name       = var.instance_name
+    CreatedBy  = "Backstage"
+  }
+}
